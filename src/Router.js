7502f1f944c8c92cb16536336a3c088c38f91f63
LeeJaeBae <nea4182@gmail.com>
2020-07-28 13:28:12 +0900

    css: header-grid

--- src/Router.js
+++ src/Router.js
@@ -2,29 +2,36 @@ import React from "react";
 import styled from "styled-components";
 import { BrowserRouter as Routes, Route } from "react-router-dom";
 import App from "./App";
-
+const Head = () => (
+	<Header>
+		<A grid="a" href="/">
+			<ButtonLogo src="/img/logo.gif" />
+		</A>
+		<A grid="b" href="/map">
+			<ButtonMap src="/img/menu01.gif" />
+		</A>
+		<A grid="c" href="/list">
+			<ButtonList src="/img/menu02.gif" />
+		</A>
+		<A grid="d" href="/creator">
+			<ButtonCreator src="/img/menu03.gif" />
+		</A>
+	</Header>
+);
 const Home = () => {
 	return (
 		<Container>
 			<img
 				src="/img/map_main.png"
 				alt=""
-				style={{ position: "absolute", left: "50%", margin: "0 0 0 -1000px", zIndex: "-1" }}
+				style={{
+					position: "absolute",
+					left: "50%",
+					margin: "0 0 0 -1000px",
+					zIndex: "-1",
+				}}
 			/>
-			<Header>
-				<a href="/">
-					<ButtonLogo src="/img/logo.gif" />
-				</a>
-				<a href="/map">
-					<ButtonMap src="/img/menu01.gif" />
-				</a>
-				<a href="/list">
-					<ButtonList src="/img/menu02.gif" />
-				</a>
-				<a href="/creator">
-					<ButtonCreator src="/img/menu03.gif" />
-				</a>
-			</Header>
+			<Head />
 		</Container>
 	);
 };
@@ -36,20 +43,7 @@ const List = () => {
 				alt=""
 				style={{ position: "absolute", left: "50%", margin: "0 0 0 -1000px", zIndex: "-1" }}
 			/>
-			<Header>
-				<a href="/">
-					<ButtonLogo src="/img/logo.gif" />
-				</a>
-				<a href="/map">
-					<ButtonMap src="/img/menu01.gif" />
-				</a>
-				<a href="/list">
-					<ButtonList src="/img/menu02.gif" />
-				</a>
-				<a href="/creator">
-					<ButtonCreator src="/img/menu03.gif" />
-				</a>
-			</Header>
+			<Head />
 		</Container>
 	);
 };
@@ -61,59 +55,46 @@ const Creator = () => {
 				alt=""
 				style={{ position: "absolute", left: "50%", margin: "0 0 0 -1000px", zIndex: "-1" }}
 			/>
-			<Header>
-				<a href="/">
-					<ButtonLogo src="/img/logo.gif" />
-				</a>
-				<a href="/map">
-					<ButtonMap src="/img/menu01.gif" />
-				</a>
-				<a href="/list">
-					<ButtonList src="/img/menu02.gif" />
-				</a>
-				<a href="/creator">
-					<ButtonCreator src="/img/menu03.gif" />
-				</a>
-			</Header>
+			<Head />
 		</Container>
 	);
 };
 
-const Container = styled.div`
-	position: relative;
-	/* overflow-x: hidden;
-	overflow-y: auto;
-	width: 100%; */
+const Container = styled.div``;
+const A = styled.a`
+	grid-area: ${(props) => props.grid};
 `;
 const Header = styled.div`
-	position: absolute;
-	top: 0;
-	height: 120px;
-	width: 100%;
+	display: grid;
+	grid-template-rows: 40px 50px 30px;
+	grid-template-columns: repeat(5, 240px);
+	grid-template-areas:
+		". . . . ."
+		"a . b c d"
+		". . . . .";
+	justify-content: center;
+	justify-items: center;
+	align-items: center;
 `;
 const ButtonMap = styled.img`
-	position: absolute;
 	width: 112px;
 	height: 23px;
 	left: 877px;
 	top: 49px;
 `;
 const ButtonList = styled.img`
-	position: absolute;
 	width: 112px;
 	height: 24px;
 	left: 1104px;
 	top: 49px;
 `;
 const ButtonCreator = styled.img`
-	position: absolute;
 	width: 106px;
 	height: 23px;
 	left: 1332px;
 	top: 49px;
 `;
 const ButtonLogo = styled.img`
-	position: absolute;
 	width: 154px;
 	height: 46px;
 	top: 37px;
